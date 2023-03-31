78f895c933ed4b04895e112a119b60e542244e4e
mahess14 <gmksvec14@gmail.com>
2023-03-31 17:42:28 +0530

    commiting test3

--- Testgit2.java
+++ Testgit2.java
@@ -1,3 +1,4 @@
+package maheshkumar;
 public class Testgit2 {
 
 	public static void main(String[] args) {
---
+++ Testgit3.java
@@ -0,0 +1,10 @@
+package maheshkumar;
+public class Testgit {
+
+	public static void main(String[] args) {
+	System.out.println("test git 3 ");
+
+	}
+
+}
+
